eb8c612f890e6a93568bdf01fb6d0656d1352681
WeMakeBest <wemakebest4you@gmail.com>
2023-06-28 11:11:15 +0530

    First Release Version (1.0)

---
+++ app/src/main/java/com/wemakebest/sampleapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.wemakebest.sampleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wemakebest.commonfunctions.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
